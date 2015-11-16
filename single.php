23afb5126c76108210183c66fcf4b2491e138e4c
Marek Podsiadły <marekpodsiadly@gmail.com>
2015-11-16 21:55:47 +0100

    comments are no visible on post

--- single.php
+++ single.php
@@ -24,9 +24,10 @@ get_header(); ?>
 			get_template_part( 'content', get_post_format() );
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
+			/*if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
+            */
 
 			// Previous/next post navigation.
 			the_post_navigation( array(
